9659c4c4a66cf095c3070f354b427b16d5fd65c9
Sighery <Sighery@users.noreply.github.com>
2023-08-15 05:25:03 +0200

    Add exercise 1-4

--- 3/main.cpp
+++ 3/main.cpp
@@ -5,7 +5,13 @@ int absolute_value(int x) {
 		return x * -1;
 }
 
+int sum(int x, int y) {
+		return x + y;
+}
+
 int main() {
 		int my_num = -10;
 		printf("The absolute value of %d is %d\n", my_num, absolute_value(my_num));
+
+		printf("Sum %d+%d=%d\n", 25, 15, sum(25, 15));
 }
